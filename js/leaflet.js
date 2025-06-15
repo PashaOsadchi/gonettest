2e7a4ff93a6097fa6028e90e0fa7c2f85805f0a4
PashaOsadchi <38135316+PashaOsadchi@users.noreply.github.com>
2025-06-15 11:42:34 +0300

    Add stub Leaflet library

--- js/leaflet.js
+++ js/leaflet.js
@@ -1,1 +1,43 @@
-// Placeholder for leaflet.js version 1.9.4
+(function(global) {
+  // Minimal Leaflet stub - offline placeholder.
+  const L = {};
+
+  L.map = function(id) {
+    return {
+      _id: id,
+      _center: [0, 0],
+      _zoom: 0,
+      setView(center, zoom) {
+        this._center = center;
+        this._zoom = zoom;
+        return this;
+      },
+      addLayer(layer) {
+        if (layer && typeof layer.addTo === 'function') {
+          layer.addTo(this);
+        }
+        return this;
+      }
+    };
+  };
+
+  L.tileLayer = function(urlTemplate, options) {
+    return {
+      addTo(map) {
+        console.log('Stub tileLayer added to map', map);
+        return this;
+      }
+    };
+  };
+
+  L.circleMarker = function(latlng, options) {
+    return {
+      addTo(map) {
+        console.log('Stub circleMarker added to map', map);
+        return this;
+      }
+    };
+  };
+
+  global.L = L;
+})(this);
