46965b7328e4042f3e0cb9635ce912dd4261f5a7
PashaOsadchi <38135316+PashaOsadchi@users.noreply.github.com>
2025-06-27 21:00:48 +0300

    Remove unused ipinfo configuration

--- js/config.js
+++ js/config.js
@@ -95,18 +95,6 @@ let speedStats = {
 
 let operator = ''
 
-let ipinfo = {
-    "ip": "", // "8.8.8.8",
-    "hostname": "", // "dns.google",
-    "city": "", // "Mountain View",
-    "region": "", // "California",
-    "country": "", // "US",
-    "loc": "", // "37.4056,-122.0775",
-    "org": "", // "AS15169 Google LLC",
-    "postal": "", // "94043",
-    "timezone": "", // "America/Los_Angeles",
-    "readme": "", // "https://ipinfo.io/missingauth"
-}
  let operators = {
     'AS21497 PrJSC VF UKRAINE': 'Vodafone',
     'AS15895 "Kyivstar" PJSC': 'Kyivstar',
