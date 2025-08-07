17468f6af9e4a95b541c051956e07b5af125c05f
PashaOsadchi <38135316+PashaOsadchi@users.noreply.github.com>
2025-08-07 11:49:56 +0300

    Add check for map.setZoom presence

--- js/map_zoom.js
+++ js/map_zoom.js
@@ -2,7 +2,9 @@ function zoomTo18() {
     if (typeof initMapIfNeeded === 'function') {
         initMapIfNeeded();
     }
-    if (map) {
+    if (map && typeof map.setZoom === 'function') {
         map.setZoom(18);
+    } else {
+        console.warn('Map instance або метод setZoom відсутні');
     }
 }
